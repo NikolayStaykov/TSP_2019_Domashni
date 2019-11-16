8e035af369a93203f1a83d67815335c637c74b97
Guru 2.0 <nikolay.staykov98@abv.bg>
2019-11-16 19:14:50 +0200

    Classes

---
+++ StreetFight/StreetFight/Fight.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFight
+{
+    class Fight
+    {
+        private Fighter Player;
+        private Fighter Oponent;
+        Random rand1;
+        public int CalcReward()
+        {
+            return (Oponent.GetHP() + Oponent.GetAtt() + Oponent.GetDef()) / 5;
+        }
+        public int FightSequence()
+        {
+            while (Player.GetHP() > 0 || Oponent.GetHP() > 0)
+            {
+                if (rand1.Next(0, 10) + Player.GetAtt() > Oponent.GetDef() + rand1.Next(0, 10))
+                {
+                    Oponent.SetHP(Oponent.GetHP() - rand1.Next(1, 4) + Player.AttackDamage());
+                }
+                if (rand1.Next(0, 10) + Player.GetDef() < rand1.Next(0, 10) + Oponent.GetAtt())
+                {
+                    Player.SetHP(Player.GetHP() - rand1.Next(1, 4) + Oponent.AttackDamage());
+                }
+            }
+            if (Player.GetHP() < 0)
+            {
+                return 0;
+            }
+            else if (Oponent.GetHP() < 0)
+            {
+                Player.SetMoney(Player.GetMoney() + CalcReward());
+                Player.SetHP(Player.GetMaxHP());
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+    }
+}
--- StreetFight/StreetFight/Fighter.cs
+++ StreetFight/StreetFight/Fighter.cs
@@ -10,19 +10,80 @@ namespace StreetFight
     {
         public string Name;
         private int HP;
+        private int MaxHP;
         private int Att;
         private int Def;
         private List<Item> Items;
+        private int Money;
+        Fighter(string N, int H, int A, int D, int M)
+        {
+            Name = N;
+            HP = H;
+            MaxHP = H;
+            Att = H;
+            Def = D;
+            Money = M;
+        }
         public void AddItem(Item I)
         {
             if (Items.Count <= 4)
             {
                 Items.Add(I);
+                MaxHP = MaxHP + I.HpBomus;
+                Att = Att + I.AttBonus;
+                Def = Def + I.DefBonus;
             }
         }
         public void SetHP(int newHP)
         {
             HP = newHP;
+        }
+        public int GetHP()
+        {
+            return HP;
+        }
+        public void SetAtt(int NewAtt)
+        {
+            Att = NewAtt;
+        }
+        public int GetAtt()
+        {
+            return Att;
+        }
+        public void SetDef(int NewDef)
+        {
+            Def = NewDef;
+        }
+        public int GetDef()
+        {
+            return Def;
+        }
+        public int GetMoney()
+        {
+            return Money;
+        }
+        public void SetMoney(int NewMoney)
+        {
+            Money = NewMoney;
+        }
+        public void SetMaxHP(int NewMaxHP)
+        {
+            MaxHP = NewMaxHP;
+        }
+        public int GetMaxHP()
+        {
+            return MaxHP;
+        }
+        public int AttackDamage()
+        {
+            if (Att - 10 < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Att - 10;
+            }
         }
     }
 }
